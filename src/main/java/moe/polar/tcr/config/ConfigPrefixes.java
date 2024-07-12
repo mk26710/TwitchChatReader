5775151c33158d8320714ecfc88257b176ff3a87
httpolar <dev@firefly.party>
2024-07-12 17:39:56 +0400

    feat: prefixes config class

---
+++ src/main/java/moe/polar/tcr/config/ConfigPrefixes.java
@@ -0,0 +1,19 @@
+package moe.polar.tcr.config;
+
+import org.jetbrains.annotations.Nullable;
+
+public class ConfigPrefixes {
+    public @Nullable String global;
+    public @Nullable String followers;
+    public @Nullable String subscribers;
+    public @Nullable String moderators;
+    public @Nullable String vips;
+
+    public ConfigPrefixes() {
+        global = null;
+        followers = null;
+        subscribers = "";
+        moderators = "";
+        vips = "";
+    }
+}
