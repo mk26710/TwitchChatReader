1f908e766786cacf2dc6dcea0b1b52996bc19996
httpolar <dev@firefly.party>
2024-07-12 18:57:04 +0400

    fix: util to support user provided colors in config

--- src/main/java/moe/polar/tcr/Utils.java
+++ src/main/java/moe/polar/tcr/Utils.java
@@ -2,6 +2,7 @@ package moe.polar.tcr;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.text.Text;
 
 import java.util.Random;
 
@@ -13,5 +14,9 @@ public class Utils {
     public static Integer randomNumIn(Integer min, Integer max) {
         Random rand = new Random();
         return rand.nextInt((max - min) + 1) + min;
+    }
+
+    public static Text colorStringToText(String s) {
+        return Text.of(s.replaceAll("&", "§"));
     }
 }
